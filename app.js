c6cf98735380ad8a820d0747a6d25fbcbe4b3bce
Hasankhan170 <hasankhanali0316@gmail.com>
2024-05-26 00:43:30 -0700

    filter product

--- app.js
+++ app.js
@@ -4,31 +4,36 @@ const products = [
         name: "Wireless Mouse",
         price: 25.99,
         category: "Electronics",
-        brand: "Logitech"
+        brand: "Logitech",
+        image: "path/to/wireless_mouse.jpg" // Replace with actual image path
     },
     {
         name: "Smartphone",
         price: 699.99,
         category: "Electronics",
-        brand: "Samsung"
+        brand: "Samsung",
+        image: "path/to/smartphone.jpg"
     },
     {
         name: "LED Monitor",
         price: 129.99,
         category: "Electronics",
-        brand: "Dell"
+        brand: "Dell",
+        image: "path/to/led_monitor.jpg"
     },
     {
         name: "Bluetooth Speaker",
         price: 45.99,
         category: "Electronics",
-        brand: "JBL"
+        brand: "JBL",
+        image: "path/to/bluetooth_speaker.jpg"
     },
     {
         name: "Laptop",
         price: 999.99,
         category: "Electronics",
-        brand: "Apple"
+        brand: "Apple",
+        image: "path/to/laptop.jpg"
     },
 
     // Footwear
@@ -36,31 +41,36 @@ const products = [
         name: "Running Shoes",
         price: 59.99,
         category: "Footwear",
-        brand: "Nike"
+        brand: "Nike",
+        image: "path/to/running_shoes.jpg"
     },
     {
         name: "Sneakers",
         price: 75.99,
         category: "Footwear",
-        brand: "Adidas"
+        brand: "Adidas",
+        image: "path/to/sneakers.jpg"
     },
     {
         name: "Sandals",
         price: 25.99,
         category: "Footwear",
-        brand: "Teva"
+        brand: "Teva",
+        image: "path/to/sandals.jpg"
     },
     {
         name: "Formal Shoes",
         price: 120.00,
         category: "Footwear",
-        brand: "Clarks"
+        brand: "Clarks",
+        image: "path/to/formal_shoes.jpg"
     },
     {
         name: "Boots",
         price: 150.00,
         category: "Footwear",
-        brand: "Timberland"
+        brand: "Timberland",
+        image: "path/to/boots.jpg"
     },
 
     // Home Appliances
@@ -68,31 +78,36 @@ const products = [
         name: "Blender",
         price: 34.99,
         category: "Home Appliances",
-        brand: "Oster"
+        brand: "Oster",
+        image: "path/to/blender.jpg"
     },
     {
         name: "Microwave Oven",
         price: 99.99,
         category: "Home Appliances",
-        brand: "Panasonic"
+        brand: "Panasonic",
+        image: "path/to/microwave_oven.jpg"
     },
     {
         name: "Air Conditioner",
         price: 299.99,
         category: "Home Appliances",
-        brand: "LG"
+        brand: "LG",
+        image: "path/to/air_conditioner.jpg"
     },
     {
         name: "Vacuum Cleaner",
         price: 149.99,
         category: "Home Appliances",
-        brand: "Dyson"
+        brand: "Dyson",
+        image: "path/to/vacuum_cleaner.jpg"
     },
     {
         name: "Toaster",
         price: 29.99,
         category: "Home Appliances",
-        brand: "Breville"
+        brand: "Breville",
+        image: "path/to/toaster.jpg"
     },
 
     // Stationery
@@ -100,31 +115,36 @@ const products = [
         name: "Notebook",
         price: 3.99,
         category: "Stationery",
-        brand: "Moleskine"
+        brand: "Moleskine",
+        image: "path/to/notebook.jpg"
     },
     {
         name: "Pen Set",
         price: 12.99,
         category: "Stationery",
-        brand: "Parker"
+        brand: "Parker",
+        image: "path/to/pen_set.jpg"
     },
     {
         name: "Highlighters",
         price: 5.99,
         category: "Stationery",
-        brand: "Sharpie"
+        brand: "Sharpie",
+        image: "path/to/highlighters.jpg"
     },
     {
         name: "Stapler",
         price: 7.99,
         category: "Stationery",
-        brand: "Swingline"
+        brand: "Swingline",
+        image: "path/to/stapler.jpg"
     },
     {
         name: "Planner",
         price: 14.99,
         category: "Stationery",
-        brand: "Erin Condren"
+        brand: "Erin Condren",
+        image: "path/to/planner.jpg"
     },
 
     // Furniture
@@ -132,31 +152,36 @@ const products = [
         name: "Desk Chair",
         price: 89.99,
         category: "Furniture",
-        brand: "Ikea"
+        brand: "Ikea",
+        image: "path/to/desk_chair.jpg"
     },
     {
         name: "Coffee Table",
         price: 129.99,
         category: "Furniture",
-        brand: "West Elm"
+        brand: "West Elm",
+        image: "path/to/coffee_table.jpg"
     },
     {
         name: "Sofa",
         price: 499.99,
         category: "Furniture",
-        brand: "Ashley Furniture"
+        brand: "Ashley Furniture",
+        image: "path/to/sofa.jpg"
     },
     {
         name: "Bookshelf",
         price: 79.99,
         category: "Furniture",
-        brand: "Sauder"
+        brand: "Sauder",
+        image: "path/to/bookshelf.jpg"
     },
     {
         name: "Dining Table",
         price: 399.99,
         category: "Furniture",
-        brand: "Pottery Barn"
+        brand: "Pottery Barn",
+        image: "path/to/dining_table.jpg"
     },
 
     // Fitness
@@ -164,31 +189,36 @@ const products = [
         name: "Yoga Mat",
         price: 19.99,
         category: "Fitness",
-        brand: "Gaiam"
+        brand: "Gaiam",
+        image: "path/to/yoga_mat.jpg"
     },
     {
         name: "Dumbbells",
         price: 49.99,
         category: "Fitness",
-        brand: "Bowflex"
+        brand: "Bowflex",
+        image: "path/to/dumbbells.jpg"
     },
     {
         name: "Treadmill",
         price: 699.99,
         category: "Fitness",
-        brand: "NordicTrack"
+        brand: "NordicTrack",
+        image: "path/to/treadmill.jpg"
     },
     {
         name: "Resistance Bands",
         price: 14.99,
         category: "Fitness",
-        brand: "Fit Simplify"
+        brand: "Fit Simplify",
+        image: "path/to/resistance_bands.jpg"
     },
     {
         name: "Exercise Bike",
         price: 299.99,
         category: "Fitness",
-        brand: "Peloton"
+        brand: "Peloton",
+        image: "path/to/exercise_bike.jpg"
     },
 
     // Kitchen Appliances
@@ -196,33 +226,118 @@ const products = [
         name: "Coffee Maker",
         price: 49.99,
         category: "Kitchen Appliances",
-        brand: "Keurig"
+        brand: "Keurig",
+        image: "path/to/coffee_maker.jpg"
     },
     {
         name: "Air Fryer",
         price: 89.99,
         category: "Kitchen Appliances",
-        brand: "Ninja"
+        brand: "Ninja",
+        image: "path/to/air_fryer.jpg"
     },
     {
         name: "Stand Mixer",
         price: 199.99,
         category: "Kitchen Appliances",
-        brand: "KitchenAid"
+        brand: "KitchenAid",
+        image: "path/to/stand_mixer.jpg"
     },
     {
         name: "Refrigerator",
         price: 799.99,
         category: "Kitchen Appliances",
-        brand: "Whirlpool"
+        brand: "Whirlpool",
+        image: "path/to/refrigerator.jpg"
     },
     {
         name: "Dishwasher",
         price: 399.99,
         category: "Kitchen Appliances",
-        brand: "Bosch"
+        brand: "Bosch",
+        image: "path/to/dishwasher.jpg"
     }
 ];
+
+const div = document.querySelector('.container');
+
+const filterItems = (btn)=>{
+    div.innerHTML = " "
+    console.log(btn.innerHTML)
+    const filtered = products.filter (item=> item.category === btn.innerHTML).map(item=>{
+        div.innerHTML += `<div class="card mt-2 p-3" style="width: 16rem; color : white; background-color : black;>
+        <div class="card-body">
+            <h5 class="card-title">Product Name: ${item.name}</h5>
+            <hr/>
+            <p class="card-text">Price: $${item.price}<br>Category: ${item.category}<br>Brand: ${item.brand}</p>
+        </div>
+    </div>`;
+    })
+}
+
+
+
+
+
+products.map(item => {
+    div.innerHTML += `<div class="card mt-2 p-3" style="width: 16rem; color : white; background-color : black;>
+        <div class="card-body">
+            <h5 class="card-title">Product Name: ${item.name}</h5>
+            <hr/>
+            <p class="card-text">Price: $${item.price}<br>Category: ${item.category}<br>Brand: ${item.brand}</p>
+        </div>
+    </div>`;
+});
+
+
+
+
+
+
+// const arrayItems = products.filter(item => item.category === 'Electronics').filter(item=>{
+//     return item.price > 200
+// })
+// console.log(arrayItems)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const arrayItems = products.filter(item=>{
 //     return item.category === 'Electronics' && item.price > 100
@@ -231,9 +346,3 @@ const products = [
 // })
 
 // console.log(arrayItems)
-
-
-const arrayItems = products.filter(item => item.category === 'Electronics').filter(item=>{
-    return item.price > 100 && item.price < 700
-})
-console.log(arrayItems)
